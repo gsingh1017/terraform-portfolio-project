b4878cc951336316a0d2ddabfb6bc1affa35b5dc
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-05-22 10:43:51 -0700

    Remove state.tf

--- nextjs-blog/terraform-nextjs/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "gs-nextjs-tf-website-state"
-    key    = "global/s3/terraform.tfstate"
-    region = "ca-central-1"
-    dynamodb_table = "gs-nextjs-website-table"
-  }
-}
